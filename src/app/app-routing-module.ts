6b230da0bb6b5bb21c757f36a47fb2c0f665dc54
Pryde <54861097+simbapryd3@users.noreply.github.com>
2021-11-10 08:43:35 -0500

    working on add to bag

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
-// import { ProductComponent } from "./product/product.component";
+import { ProductComponent } from "./product/product.component";
 import { ProductListComponent } from "./product-list/product-list.component";
 import { CustomerComponent } from "./customer/customer.component";
 import { BillingStatementComponent } from "./billing-statement/billing-statement.component";
 
 const routes: Routes = [
     {path:'', redirectTo: '', pathMatch: 'full'},
-    {path: 'viewProducts', component: ProductListComponent},
+    {path: 'viewProducts', component: ProductComponent},
     {path:"customers",component:CustomerComponent},
     {path:"billingStatement",component:BillingStatementComponent}
 
--- src/app/mock-food.ts
+++ src/app/mock-food.ts
@@ -1,8 +1,12 @@
+
 import { Food } from './food';
 
 export const FOODS: Food[] = [
   { price: '$11', name: 'Hamburger' , url: "https://assets.epicurious.com/photos/57c5c6d9cf9e9ad43de2d96e/master/w_1280,c_limit/the-ultimate-hamburger.jpg"},
-  { price: '$5', name: 'Tacos', url: "https://images-gmi-pmc.edge-generalmills.com/e59f255c-7498-4b84-9c9d-e578bf5d88fc.jpg" },
+  { price: '$5', name: 'Tacos', url: "https://d47gxy3fatgwk.cloudfront.net/wp-content/uploads/2021/08/ground-beef-tacos-570-1200x800.jpg" },
  { price: '$14', name: 'Pizza', url: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-keto-pizza-073-1544039876.jpg" },
+ { price: "$12", name: "Chicken Wings", url: "https://assets.epicurious.com/photos/57d6d3ce219ffa4a2747eea5/master/w_1280,c_limit/sweet-chili-glazed-chicken-wings.jpg"},
+
+ { price: "$3", name: "Drink", url: "https://cdn.vox-cdn.com/thumbor/sy2GXgGVHD9QRsZDZc79a30CxHU=/245x0:3266x2266/1220x813/filters:focal(245x0:3266x2266):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/48794543/soda-breakfast-g.0.0.jpg"}
 
 ];
